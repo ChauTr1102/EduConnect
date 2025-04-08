86e0c52ed3c44675faa8780549d2950370245b99
Bojjoo <superappolo@gmail.com>
2025-04-08 14:50:25 +0700

    fix database ver 1.2

--- init.sql
+++ init.sql
@@ -12,61 +12,61 @@ CREATE TABLE users(
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     name text not null,
     birth_date date not null,
-    gender varchar(6) not null check(gender in ('Male', 'Female'))
+    gender varchar(6) not null check(gender in ('Male', 'Female')),
     email VARCHAR(100) UNIQUE,
-    address text,
-
+    address text
 );
 
 CREATE TABLE students(
-    user_id varchar(20) references user(user_id) primary key ON DELETE CASCADE,
+    user_id varchar(20) references users(user_id) ON DELETE CASCADE primary key
 );
 
-CREATE TABLE Teachers (
-    user_id varchar(20) REFERENCES Users(id) PRIMARY KEY ON DELETE CASCADE,
+CREATE TABLE teachers (
+    user_id varchar(20) REFERENCES users(user_id) ON DELETE CASCADE PRIMARY KEY,
     avg_rate NUMERIC(3,2) DEFAULT 0
 );
 
-CREATE TABLE Subjects (
-    id varchar(20) PRIMARY KEY,
+CREATE TABLE subjects (
+    subject_id varchar(20) PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Classes (
-    id varchar(20) PRIMARY KEY,
+CREATE TABLE classes (
+    class_id varchar(20) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    subject_id varchar(20) REFERENCES Subjects(id) ON DELETE SET NULL
+    subject_id varchar(20) REFERENCES subjects(subject_id) ON DELETE SET NULL
 );
 
-CREATE TABLE Class_Student (
-    student_id varchar(20) REFERENCES Students(user_id) ON DELETE CASCADE,
-    class_id varchar(20) REFERENCES Classes(id) ON DELETE CASCADE,
+CREATE TABLE class_Student (
+    student_id varchar(20) REFERENCES students(user_id) ON DELETE CASCADE,
+    class_id varchar(20) REFERENCES classes(class_id) ON DELETE CASCADE,
     PRIMARY KEY (student_id, class_id)
 );
 
-CREATE TABLE Reviews (
-    id varchar(20) PRIMARY KEY,
-    student_id varchar(20) REFERENCES Students(user_id) ON DELETE CASCADE,
-    teacher_id varchar(20) REFERENCES Teachers(user_id) ON DELETE CASCADE,
+CREATE TABLE reviews (
+    review_id varchar(20) PRIMARY KEY,
+    student_id varchar(20) REFERENCES students(user_id) ON DELETE CASCADE,
+    teacher_id varchar(20) REFERENCES teachers(user_id) ON DELETE CASCADE,
     rate INT CHECK (rate >= 0 AND rate <= 5),
     comment TEXT,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Posts (
-    id varchar(20) PRIMARY KEY,
-    user_id varchar(20) REFERENCES Users(id) ON DELETE CASCADE,
-    content TEXT NOT NULL,
-    status VARCHAR(50) DEFAULT 'pending' check(status in 'pending', 'matched')
+CREATE TABLE posts (
+    post_id varchar(20) PRIMARY KEY,
+    user_id varchar(20) REFERENCES users(user_id) ON DELETE CASCADE,
+    content TEXT NOT NULL
 );
 
 -- LOGS_AI_MATCHING
-CREATE TABLE Logs_AI_matching (
-    id varchar(20) PRIMARY KEY,
-    user_id varchar(20) REFERENCES Users(id) ON DELETE CASCADE,
-    class_id varchar(20) REFERENCES Classes(id) ON DELETE SET NULL,
-    post_id varchar(20) REFERENCES Posts(id) ON DELETE SET NULL,
+CREATE TABLE logs_AI_matching (
+    matching_log_id varchar(20) PRIMARY KEY,
+    student_id varchar(20) REFERENCES students(user_id) ON DELETE CASCADE,
+	teacher_id varchar(20) REFERENCES teachers(user_id) ON DELETE CASCADE,
+    class_id varchar(20) REFERENCES classes(class_id) ON DELETE SET NULL,
+    post_id varchar(20) REFERENCES posts(post_id) ON DELETE SET NULL,
+	status VARCHAR(50) DEFAULT 'pending' check(status in ('pending', 'matched')),
     matched_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (post_id, status)
+	UNIQUE (post_id, status)
 );
 
