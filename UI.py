43ae3da048048ebd9556890c689ce9feef0de518
Ngô Đức Mạnh <ducmanh4009@gmail.com>
2025-04-09 16:08:40 +0700

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,1 +1,8 @@
-print("hello")
+import psycopg2
+conn=psycopg2.connect(database='Educonnect',host='localhost', port= '5432', user='postgres', password='12345678')
+cur = conn.cursor()
+conn.set_session(autocommit = True)
+
+cur.execute("select * from users")
+a = cur.fetchall()
+print(len(a))
