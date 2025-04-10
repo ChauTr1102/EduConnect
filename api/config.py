b04fa211fc4550c812ad5e58fe509d4337e51769
Bojjoo <superappolo@gmail.com>
2025-04-10 12:43:21 +0700

    add main.py

--- api/config.py
+++ api/config.py
@@ -1,3 +1,14 @@
+RECOMMENDER_PROMPT = """## Task:
+1. Analyze the student's request to identify key requirements such as subject, degree, experience, schedule, teaching style, etc.
+2. Evaluate each teacher profile against these requirements.
+3. Select the most suitable teachers (maximum of 3), and briefly explain why each one is a good match.
+
+## Output:
+- Matching Teachers:
+1. [Teacher Name] – Reason for the match
+2. [Teacher Name] – Reason for the match
+3. [Teacher Name] – Reason for the match"""
+
 DATABASE = 'Educonnect'
 HOST = 'postgres'
 PORT = '5432'
--- api/services/recommender_system.py
+++ api/services/recommender_system.py
@@ -1,17 +1,17 @@
 from api.services import *
 
+
 class RecommenderSystem:
     def __init__(self, gemini_apikey=None):
         self.gemini_apikey = gemini_apikey
 
-    def prompt_recommender(self):
+    def prompt_recommender(self, student_request, teacher_profiles):
         prompt_recommender = ChatPromptTemplate.from_messages(
             [
-                ("system", "**Instruction 1:**\n {prompt_folder}\n"),
-                ("system", "**Instruction 2:**\n {prompt_template}\n"),
-                ("system", "**Chat history:**\n {chat_history}\n"),
-                ("system", "**Context about relevant data:**\n{context}\n"),
-                ("system", "**Context received from google search:**\n{web_search_context}\n"),
+                ("system", "You are an AI system tasked with finding the most suitable teachers based on a student's request."),
+                ("system", f"{RECOMMENDER_PROMPT}"),
                 ("system", "**Answer the question:** {question}"),
+                ("system", "**Student's Request**: {student_request}"),
+                ("system", "**List of Available Teachers:**\n{teacher_profiles}\n"),
             ]
         )
