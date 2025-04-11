a4e69b2a8ae9c410c6a92f93bba100b9befe1be9
Bojjoo <superappolo@gmail.com>
2025-04-11 23:10:08 +0700

    nothin

--- api/config.py
+++ api/config.py
@@ -2,12 +2,18 @@ RECOMMENDER_PROMPT = """## Task:
 1. Analyze the student's request to identify key requirements such as subject, degree, experience, schedule, teaching style, etc.
 2. Evaluate each teacher profile against these requirements.
 3. Select the most suitable teachers (maximum of 3), and briefly explain why each one is a good match.
-
-## Output:
-- Matching Teachers:
-1. [Teacher_id - Teacher Name - teacher mail] – Reason for the match
-2. [Teacher_id - Teacher Name - teacher mail] – Reason for the match
-3. [Teacher_id - Teacher Name - teacher mail] – Reason for the match"""
+4. If the user's input is not about finding teacher, just response nothing ""
+## Output (Return the result in the following JSON format):
+[
+  {{
+    "teacher_id": teacher_id,
+    "name": teacher_name,
+    "email": email
+    "reason": "Short explanation why this teacher is a good fit"
+  }},
+    ...
+]
+"""
 
 DATABASE_CONFIG = {
     "dbname": "Educonnect",
