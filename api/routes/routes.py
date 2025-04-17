687389f3ee06cc897487af3021e7c9368d088dc4
Bojjoo <superappolo@gmail.com>
2025-04-17 14:38:37 +0700

    fix html chatbot

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -1,15 +1,18 @@
 from api.routes import *
 
+
 @router.get("/")
 def read_root():
     return {
         'Hello This is EduConnect! Nice to meet you!'
     }
 
+
 @router.post("/check_login")
 async def check_login(test: user_and_pass):
     result = sql_db.check_login(test.user_name, test.password)
     return result
+
 
 @router.post("/recommender/")
 async def recommender(student_request: StudentRequest):
