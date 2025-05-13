88fc5ec92f9bf63fd0e81cd3b261d172f749104e
Ngô Đức Mạnh <ducmanh4009@gmail.com>
2025-05-13 12:52:21 +0700

    Update home.js

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -290,7 +290,7 @@ document.addEventListener('DOMContentLoaded', function() {
         setTimeout(() => {
             const newPostData = {
                 user: "user",
-                avatar: "images\images\4.png",
+                avatar: "images/images/4.png",
                 time: "Just now",
                 content: content,
                 id: Date.now()
